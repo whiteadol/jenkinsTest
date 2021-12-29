dcf43361214deb390bcb87dbf0507427a81957bc
whiteadol <whiteadol3@naver.com>
2021-12-29 15:46:36 +0900

    webhook test

---
+++ src/test/java/com/example/jenkinstest/temp/TempTests.java
@@ -0,0 +1,21 @@
+package com.example.jenkinstest.temp;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TempTests {
+
+    @Test
+    void testV1() {
+
+        List<String> list = new ArrayList<>();
+
+//        String str = list.get(0);
+//
+//        System.out.println(str);
+
+        System.out.println(list);
+    }
+}
