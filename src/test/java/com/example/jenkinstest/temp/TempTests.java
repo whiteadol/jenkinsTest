73a3d003eb1cb45a37a4cda4eb7441bf0fdee109
whiteadol <whiteadol3@naver.com>
2021-12-29 15:01:16 +0900

    jenkins error test

---
+++ src/test/java/com/example/jenkinstest/temp/TempTests.java
@@ -0,0 +1,19 @@
+package com.example.jenkinstest.temp;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TempTests {
+
+    @Test
+    void testV1p(){
+
+        List<String> list = new ArrayList<>();
+
+        String str = list.get(0);
+
+        System.out.println(str);
+    }
+}
